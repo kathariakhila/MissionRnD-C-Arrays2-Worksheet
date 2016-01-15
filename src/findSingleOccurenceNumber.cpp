80ea3329b97f17538fbaeca5883a4ccaf6a5483b
kathariakhila <kathariakhila@missionrnd.com>
2016-01-15 19:42:48 +0530

    Submit Commit

--- src/findSingleOccurenceNumber.cpp
+++ src/findSingleOccurenceNumber.cpp
@@ -13,6 +13,56 @@ ERROR CASES: Return -1 for invalid inputs.
 NOTES:
 */
 
+#include<iostream>
+
+int check(int number, int *temp,int l){
+
+	int i;
+	
+
+	if (l == 0){
+		return -1;
+	}
+	else {
+		for (i = 0; i < l; i++){
+			if (number == temp[i])
+				return i;
+		}
+		return -1;
+	}
+
+}
+
 int findSingleOccurenceNumber(int *A, int len) {
-	return -1;
+
+	int res;
+	int temp[10] = {0};
+	int count[10] = { 0 };
+	int l = 0;
+	int i;
+	int flag;
+
+	if (A == NULL)
+		return -1;
+	
+	else{
+		
+		for (i = 0; i < len; i++){
+			flag = check(A[i], temp,l);
+		
+			if (flag == -1){
+				temp[l] = A[i];
+				count[l] = 1;
+				l++;
+			}
+			else{
+				count[flag] = count[flag] + 1;
+			}
+		}
+		for (i = 0; i < l; i++)
+			if (count[i] == 1)
+				return temp[i];
+
+
+	}
 }
